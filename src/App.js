b7fa84a2522976229d1a572a9852cc047cbd8b73
Ihab Ali <ihab@live.com>
2019-07-11 22:25:57 -0500

    navbar

--- src/App.js
+++ src/App.js
@@ -1,21 +1,24 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
-import Home from './components/Home';
-import Login from './components/Login/Login';
+import Home from "./components/Home";
+import About from "./components/About";
+import Login from "./components/Login/Login";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 class App extends React.Component {
   state = {
     loggedIn: false
-  }
+  };
 
   render() {
     return (
       <Router>
         <Switch>
           <Route exact path="/" render={() => <Home />} />
-          <Route path="/login" render={() => <Login />} /> { /* Register is a nested Route */ }
-          <Route path="/about" render={() => <></>} />
+          <Route path="/login" render={() => <Login />} />{" "}
+          {/* Register is a nested Route */}
+          <Route path="/about" render={() => <About />} />
           <Route path="/contact" render={() => <></>} />
           <Route path="/profile" render={() => <></>} />
         </Switch>
