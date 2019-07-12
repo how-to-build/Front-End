8116c4d6fbf31436f72801f34d397f5a5f63f2db
ChristianMacDonald <chrismacd412@gmail.com>
2019-07-11 21:32:29 -0400

    Deleted some files

--- src/components/Login/Login.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-class Login extends React.Component {
-  
-}
-
-export default Login;
--- src/components/Login/RegisterForm.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-class RegisterForm extends React.Component {
-
-}
-
-export default RegisterForm;
