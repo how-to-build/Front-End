0ffaa882604126edb84a6fa561a9e5a944528568
ChristianMacDonald <chrismacd412@gmail.com>
2019-07-11 22:03:30 -0400

    Commented out Login component

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 import Home from './components/Home';
-import Login from './components/Login/Login';
+//import Login from './components/Login/Login';
 
 class App extends React.Component {
   state = {
@@ -14,7 +14,7 @@ class App extends React.Component {
       <Router>
         <Switch>
           <Route exact path="/" render={() => <Home />} />
-          <Route path="/login" render={() => <Login />} /> { /* Register is a nested Route */ }
+          <Route path="/login" render={() => <></>} /> { /* Register is a nested Route */ }
           <Route path="/about" render={() => <></>} />
           <Route path="/contact" render={() => <></>} />
           <Route path="/profile" render={() => <></>} />
