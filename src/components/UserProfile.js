54a734e1bb61a2c609ef3294342e8dd4a33eae42
ChristianMacDonald <chrismacd412@gmail.com>
2019-07-15 22:32:00 -0400

    Added tabs to ProfilePage for User Info, Created HowTos, and Liked HowTos

--- src/components/UserProfile.js
+++ src/components/UserProfile.js
@@ -2,37 +2,76 @@ import React from 'react';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
 
-import { Card, CardBody, CardTitle, CardText, Button, FormGroup, Label, Input } from 'reactstrap';
+import classnames from 'classnames';
+
+import { Card, CardBody, CardTitle, CardText, Button, FormGroup, Label, Input, Nav, NavItem, NavLink, TabContent, TabPane } from 'reactstrap';
 import defaultAvatar from '../assets/defaultavatar.jpg';
 
-const UserProfile = props => {
-  return (
-    <div>
-      <Header />
-      <Card className="col-md-5 col-sm-10 mx-auto mb-4">
-        <CardBody>
-          <CardTitle><h1>Profile</h1></CardTitle>
-        </CardBody>
-        <CardBody className="text-center">
-          <img src={defaultAvatar} alt="Profile Img" className="img-thumbnail mx-auto d-block" />
-          <CardText>{`${props.user.first_name} ${props.user.last_name}`}</CardText>
-          <CardText>{props.user.email}</CardText>
-        </CardBody>
-        <CardBody>
-          <FormGroup>
-            <Label for="username">Username</Label>
-            <Input disabled type="text" name="username" value={props.user.username} />
-          </FormGroup>
-          <FormGroup>
-            <Label for="password">Password</Label>
-            <Input disabled type="text" name="password" value={props.user.password} />
-          </FormGroup>
-          <Button color="secondary" onClick={props.handleClick}>Change Password</Button>
-        </CardBody>
-      </Card>
-      <Footer />
-    </div>
-  );
+class UserProfile extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      activeTab: '1'
+    }
+    this.toggle = this.toggle.bind(this);
+  }
+
+  toggle(tab) {
+    if (this.state.activeTab !== tab) {
+      this.setState({ activeTab: tab });
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        <Header />
+        <Card className="col-md-5 col-sm-10 mx-auto mb-4">
+          <CardBody>
+            <CardTitle><h1>Profile</h1></CardTitle>
+          </CardBody>
+          <Nav tabs>
+            <NavItem>
+              <NavLink className={classnames({ active: this.state.activeTab === '1'})} onClick={() => { this.toggle('1'); }}>Profile Info</NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink className={classnames({ active: this.state.activeTab === '2'})} onClick={() => { this.toggle('2'); }}>Created HowTos</NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink className={classnames({ active: this.state.activeTab === '3'})} onClick={() => { this.toggle('3'); }}>Liked HowTos</NavLink>
+            </NavItem>
+          </Nav>
+          <TabContent activeTab={this.state.activeTab}>
+            <TabPane tabId="1">
+              <CardBody className="text-center">
+                <img src={defaultAvatar} alt="Profile Img" className="img-thumbnail mx-auto d-block" />
+                <CardText>{`${this.props.user.first_name} ${this.props.user.last_name}`}</CardText>
+                <CardText>{this.props.user.email}</CardText>
+              </CardBody>
+              <CardBody>
+                <FormGroup>
+                  <Label for="username">Username</Label>
+                  <Input disabled type="text" name="username" value={this.props.user.username} />
+                </FormGroup>
+                <FormGroup>
+                  <Label for="password">Password</Label>
+                  <Input disabled type="text" name="password" value={this.props.user.password} />
+                </FormGroup>
+                <Button color="secondary" onClick={this.props.handleClick}>Change Password</Button>
+              </CardBody>
+            </TabPane>
+            <TabPane tabId="2">
+              <h3>HowTos that the user has created will show up here!</h3>
+            </TabPane>
+            <TabPane tabId="3">
+              <h3>HowTos that the user has liked will show up here!</h3>
+            </TabPane>
+          </TabContent>
+        </Card>
+        <Footer />
+      </div>
+    );
+  }
 }
 
 UserProfile.defaultProps = {
