014a5ec31eb97f0789b583db55c6480071109bdb
Ihab Ali <ihab@live.com>
2019-07-16 21:59:43 -0500

    implement CardsList into howToCard to apply map method

--- src/components/Cards/CardsList.js
+++ src/components/Cards/CardsList.js
@@ -1,14 +1,6 @@
 import React from "react";
 import { connect } from "react-redux";
-import {
-  Card,
-  CardBody,
-  Button,
-  CardTitle,
-  CardText,
-  CardImg,
-  Spinner
-} from "reactstrap";
+import { Spinner } from "reactstrap";
 
 import HowToCard from "./howToCard";
 
@@ -27,29 +19,14 @@ class CardsList extends React.Component {
   render() {
     return (
       <div className="d-flex flex-row flex-wrap align-items-start justify-content-start mt-4 mb-4">
-        <Card className="col-5 m-2">
-          <CardImg
-            top
-            width="100%"
-            src="https://laurenpoussard.com/wp-content/uploads/2017/07/How-to-lauren-poussard-creative-studio-danvers.jpg"
-            alt="Card image cap"
-          />
-          <CardBody>
-            <CardTitle>Card Title</CardTitle>
-            <CardText>Card Text</CardText>
-            <CardText>
-              <small className="text-muted">Last updated 3 mins ago</small>
-            </CardText>
-          </CardBody>
-        </Card>
-        {/* {this.props.isLoading ? (
+        {this.props.isLoading ? (
           <Spinner type="grow" color="secondary" />
         ) : null}
         {this.props.cards.length >= 1
           ? this.props.cards.map(card => (
               <HowToCard key={card.id} card={card} />
             ))
-          : null} */}
+          : null}
       </div>
     );
   }
--- src/components/Cards/howToCard.js
+++ src/components/Cards/howToCard.js
@@ -1,19 +1,29 @@
 import React from "react";
-import { Card, CardImg, CardBody, CardTitle, CardText }  from 'reactstrap';
+import { Card, CardImg, CardBody, CardTitle, CardText } from "reactstrap";
+import howtoimg from "../../assets/howtoimg.jpg";
 
 const HowToCard = props => {
   return (
-    <Card className="col-10 col-md-3 col-lg-3 col-xl-3">
-      <CardImg />
+    <Card className="col-5 m-2">
+      <CardImg
+        className="m-2 mx-auto d-block"
+        top
+        width="100%"
+        src={howtoimg}
+        alt="Card image cap"
+      />
       <CardBody>
         <CardTitle>{props.card.title}</CardTitle>
         <CardText>{props.card.description}</CardText>
         <CardText>
           <small className="text-muted">Likes {props.card.likes}</small>
         </CardText>
+        <CardText>
+          <small className="text-muted">Last updated 3 mins ago</small>
+        </CardText>
       </CardBody>
     </Card>
-  )
+  );
 };
 
 export default HowToCard;
