65f6003b039e8ca91d188084eaec39e00f177955
ChristianMacDonald <chrismacd412@gmail.com>
2019-07-16 23:50:15 -0400

    Got rid of password on User Info tab as well as created new reducer for ChangePassword actions

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -3,11 +3,13 @@ import { combineReducers } from 'redux';
 import { LoggingIn } from '../reducers/Login';
 import { Registering } from '../reducers/Register';
 import { getCards } from '../reducers/getCards';
+import { ChangePassword } from '../reducers/ChangePassword';
 
 const rootReducer = combineReducers({
   LoggingIn,
   Registering,
-  getCards
+  getCards,
+  ChangePassword
 })
 
 export default rootReducer;
