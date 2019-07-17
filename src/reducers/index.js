7c1b8bec95fada6f20c50fb0b5f3ae1c60caf069
ChristianMacDonald <chrismacd412@gmail.com>
2019-07-16 23:53:13 -0400

    Corrected name of reducer in index.js

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -3,13 +3,13 @@ import { combineReducers } from 'redux';
 import { LoggingIn } from '../reducers/Login';
 import { Registering } from '../reducers/Register';
 import { getCards } from '../reducers/getCards';
-import { ChangePassword } from '../reducers/ChangePassword';
+import { ChangingPassword } from '../reducers/ChangePassword';
 
 const rootReducer = combineReducers({
   LoggingIn,
   Registering,
   getCards,
-  ChangePassword
+  ChangingPassword
 })
 
 export default rootReducer;
